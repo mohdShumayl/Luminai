0b2df032c467628545c5d3a8cc547b32c245d73e
Meesum Raza <mail2meesum@gmail.com>
2025-10-05 01:43:48 +0530

    first commit

---
+++ app/(main)/dashboard/page.jsx
@@ -0,0 +1,20 @@
+import { getUserOnboardingStatus } from "@/actions/user";
+import { redirect } from "next/navigation";
+
+const IndustryInsightsPage = async () => {
+  const { isOnboarded } = await getUserOnboardingStatus();
+
+  // If not onboarded, redirect to onboarding page
+  // Skip this check if already on the onboarding page
+  if (!isOnboarded) {
+    redirect("/onboarding");
+  }
+
+  return (
+    <div className="container mx-auto">
+      IndustryInsightsPage
+    </div>
+  );
+}
+
+export default IndustryInsightsPage;
