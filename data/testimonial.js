4e4fd3c0af4a0c3efd65b08e3f9f5a28f6f940d1
mohdShumayl <shumail582k@gmail.com>
2025-10-10 01:44:30 +0530

    landingpage

--- data/testimonial.js
+++ data/testimonial.js
@@ -1,12 +1,10 @@
-import imageOne from "@/public/74.jpg"
-import imagetwo from "@/public/75.jpg"
-import imagethree from "@/public/751.jpg"
+
 export const testimonial = [
   {
     quote:
       "LuminAI completely transformed how I approach my career. The AI suggestions are spot-on, and I now feel confident making strategic career moves!",
     author: "Sarah Chen",
-    image: "https://github.com/mohdShumayl/Luminai/blob/main/public/74.jpg",
+    image: "https://xsgames.co/randomusers/avatar.php?g=female",
     role: "Software Engineer",
     company: "Tech Giant Co.",
   },
@@ -14,15 +12,15 @@ export const testimonial = [
     quote:
       "LuminAI’s personalized insights helped me discover opportunities I never considered. Highly recommend!",
     author: "Michael Rodriguez",
-    image: "https://github.com/mohdShumayl/Luminai/blob/main/public/751.jpg",
+    image: "https://xsgames.co/randomusers/avatar.php?g=male",
     role: "Product Manager",
     company: "StartUp Inc.",
   },
   {
     quote:
       "The app’s AI-driven guidance is incredible. It’s like having a career coach available 24/7, and it helped me prepare for skill growth.",
-    author: "Disuza",
-    image: "https://github.com/mohdShumayl/Luminai/blob/main/public/75.jpg",
+    author: "Christina Howell",
+    image: "https://xsgames.co/randomusers/avatar.php?g=pixel",
     role: "Marketing Director",
     company: "Global Corp",
   },
